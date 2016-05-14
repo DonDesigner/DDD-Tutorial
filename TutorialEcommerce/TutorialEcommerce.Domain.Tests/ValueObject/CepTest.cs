4b849c86bf9f1c6be7f6f2477e2a20233539ea61
Diego Fernandes <dondesigner@hotmail.com>
2016-05-14 13:55:57 -0300

    Criado os Testes para o CEP

---
+++ TutorialEcommerce/TutorialEcommerce.Domain.Tests/ValueObject/CepTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TutorialEcommerce.Domain.ValueObject;
+
+namespace TutorialEcommerce.Domain.Tests.ValueObject
+{
+    [TestClass]
+    public class CepTest
+    {
+        [TestMethod]
+        public void Cep_Valido()
+        {
+            var cep = "14401-229";
+            var obj = new Cep(cep);
+            Assert.AreEqual(14401229, obj.CepCod);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Cep_InValido()
+        {
+            var cep = "asfsaf";
+            var obj = new Cep(cep);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Cep_EmBranco()
+        {
+            new Cep("");
+        }
+
+        [TestMethod]
+        public void Cep_GetCepFormatado14401229()
+        {
+            var cep = "14401-229";
+            var obj = new Cep(cep);
+            Assert.AreEqual(cep, obj.GetCepFormatado());
+        }
+
+        [TestMethod]
+        public void Cep_GetCepFormatado00000000()
+        {
+            var cep = "00000-000";
+            var obj = new Cep(cep);
+            Assert.AreEqual(cep, obj.GetCepFormatado());
+        }
+
+        [TestMethod]
+        public void Cep_GetCepFormatado12345678()
+        {
+            var cep = "12345-678";
+            var obj = new Cep(cep);
+            Assert.AreEqual(cep, obj.GetCepFormatado());
+        }
+      
+
+    }
+}
