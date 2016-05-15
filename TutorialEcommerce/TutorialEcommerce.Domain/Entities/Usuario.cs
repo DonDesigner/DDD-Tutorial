f2e0afb5e579fbbadbd8d3d918ee8f0055dcc0af
Diego Fernandes <dondesigner@hotmail.com>
2016-05-14 22:14:20 -0300

    Concluido

--- TutorialEcommerce/TutorialEcommerce.Domain/Entities/Usuario.cs
+++ TutorialEcommerce/TutorialEcommerce.Domain/Entities/Usuario.cs
@@ -22,6 +22,7 @@ namespace TutorialEcommerce.Domain.Entities
         public const int SenhaMinValue = 6;
         public const int SenhaMaxValue = 30;
         public byte[] Senha { get; private set; }
+
         public Guid TokenAlteracaoDeSenha { get; private set; }
 
         //Construtor EntityFramework
@@ -39,7 +40,6 @@ namespace TutorialEcommerce.Domain.Entities
         public void SetLogin(string login)
         {
             Guard.ForNullOrEmptyDefaultMessage(login, "Login");
-            //TODO: Problemas com StringLength
             Guard.StringLength("Login", login, LoginMinValue, LoginMaxValue);
 
             Login = login;
@@ -65,9 +65,39 @@ namespace TutorialEcommerce.Domain.Entities
         {
             Guard.ForNullOrEmptyDefaultMessage(senha, "Senha");
             Guard.ForNullOrEmptyDefaultMessage(senhaConfirmacao, "Confirmação de Senha");
+            Guard.StringLength("Senha", senha, SenhaMinValue, SenhaMaxValue);
+            Guard.AreEqual(senha, senhaConfirmacao, "As senhas não conferem!");
 
             Senha = CriptografiaHelper.CriptografarSenha(senha);
         }
 
+        public void AlterarSenha(string senhaAtual, string novaSenha, string confirmacaoDeSenha)
+        {
+            ValidarSenha(senhaAtual);
+            SetSenha(novaSenha, confirmacaoDeSenha);
+        }
+
+        public void ValidarSenha(string senha)
+        {
+            Guard.ForNullOrEmptyDefaultMessage(senha, "Senha");
+            var senhaCriptografada = CriptografiaHelper.CriptografarSenha(senha);
+            if (!Senha.SequenceEqual(senhaCriptografada))
+                throw new Exception("Senha inválida!");
+        }
+
+        public Guid GerarNovoTokenAlterarSenha()
+        {
+            TokenAlteracaoDeSenha = Guid.NewGuid();
+            return TokenAlteracaoDeSenha;
+        }
+
+        public void AlterarSenha(Guid token, string novaSenha, string confirmacaoDeSenha)
+        {
+            if (!TokenAlteracaoDeSenha.Equals(token))
+                throw new Exception("token para alteração de senha inválido!");
+
+            SetSenha(novaSenha, confirmacaoDeSenha);
+            GerarNovoTokenAlterarSenha();
+        }
     }
 }
