5ff2375c1184603d4e6a27017a61218a43322e6f
DonDesigner <dondesigner@hotmail.com>
2016-05-20 18:22:55 -0300

    Iniciando Entity Framework

---
+++ TutorialEcommerce/TutorialEcommerce.Repositories/EntityTypeConfigurations/UsuarioConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using TutorialEcommerce.Domain;
+
+
+namespace TutorialEcommerce.Repositories.EntityTypeConfigurations
+{
+    public class UsuarioConfigurations : EntityTypeConfigurations<Usuario>
+    {
+    }
+}
